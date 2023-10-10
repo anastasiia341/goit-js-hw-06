1e31b12a7f62e38956a8d3b913fd247c54dc6618
anastasiia341 <135730140+anastasiia341@users.noreply.github.com>
2023-10-10 16:57:05 +0200

    task-6, task-8

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,11 @@ const inputValidation = inputEl.getAttribute('data-length');
 inputEl.addEventListener('blur',onInputValidation)
 
 function onInputValidation(event) {
-    if (event.currentTarget.value.length >= inputValidation) {
+    if (event.currentTarget.value.length === Number(inputValidation)) {
         inputEl.classList.add('valid');
         inputEl.classList.remove('invalid');
     } else {
         inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
     }
 }
--- js/task-08.js
+++ js/task-08.js
@@ -6,7 +6,8 @@ function onSubmit(event) {
     event.preventDefault();
     const { email, password } = event.currentTarget.elements;
     if (!email.value || !password.value ) {
-        alert('Ви повинні заповнити всі поля!')
+        alert('Ви повинні заповнити всі поля!');
+        return
     }
     console.log(email.value);
     console.log(password.value);
